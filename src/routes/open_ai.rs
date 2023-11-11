8d3f7360d714266c4ed8de4be7f9906d7a50a28a
Usairim Isani <usairim.isani@gmail.com>
2023-11-11 12:01:03 +0200

    refactor: deploy

--- src/routes/open_ai.rs
+++ src/routes/open_ai.rs
@@ -5,6 +5,7 @@ use crate::error::Error;
 use crate::extract::Json;
 use openai_api_rs::v1::api::Client;
 use openai_api_rs::v1::chat_completion::{self, ChatCompletionRequest};
+use openai_api_rs::v1::common::GPT3_5_TURBO;
 use serde::{Deserialize, Serialize};
 use tokio::fs;
 use tokio::io::AsyncWriteExt;
@@ -40,7 +41,7 @@ pub async fn open_ai(
 ) -> Result<Json<OpenAIPromptResponse>, Error> {
     let openai_client = Client::new(state.env.openai_api_key().to_string());
     let req = ChatCompletionRequest::new(
-        chat_completion::GPT3_5_TURBO.to_string(),
+        GPT3_5_TURBO.to_string(),
         vec![chat_completion::ChatCompletionMessage {
             role: chat_completion::MessageRole::user,
             content,
